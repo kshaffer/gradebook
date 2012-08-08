3f31d2828fdf1ced052bf4148c6ce3591bab4ebf
Kris Shaffer <kris.shaffer@gmail.com>
2012-08-08 10:45:14 -0400

    can generate rudimentary reports for all students in folder

--- gradebook.py
+++ gradebook.py
@@ -21,6 +21,8 @@ import csv
 import sys
 import os
 
+instructor = 'Kris P. Shaffer'
+
 
 class Student():
 
@@ -29,6 +31,9 @@ class Student():
 
 		self.filename = filename
 		self.name = filename.split('.')[0]
+		self.latexHeader = 'latex input: mmd-article-header\n' + 'Title: Grade report for ' + self.name + '\n' + 'Author: ' + instructor + '\n' + 'Base Header Level: 2' + '\n' + 'latex mode: memoir' + '\n' + 'latex input: mmd-article-begin-doc' + '\n' + 'latex footer: mmd-memoir-footer\n\n'
+
+
 
 	def assessments(self):
 		f = open(self.filename, 'rb')
@@ -50,5 +55,62 @@ class Student():
 			categoryList.append(assessment[1])
 		return sorted(set(categoryList))
 		
-#	def categoryTable(self, category):
+	def categoryTable(self, category):
+		table = '| Objective | Status | Scale | Assessment 1 | Date 1 | Assessment 2 | Date 2 |\n'
+		table += '| --: | :-: | :-: | :-: | :-: | :-: | :-: |\n'
+		for assess in self.assessments():
+			if assess[1] == category:
+				table += ("| " + assess[0] + " | " + assess[2] + " | " + assess[3] + " | " + assess[4] + " | " + assess[5] + " | " + assess[6] + " | " + assess[7] + " |\n")
+		table += '[Assessments for ' + category + ']\n'
+		return table
 		
+	def report(self, course):
+		report = self.latexHeader + '\n'
+		for category in course.categories():
+			report += self.categoryTable(category)
+			report += '\n\n'
+		return report
+		
+	def writeReport(self, course):
+		reportFile = self.name.split('.')[0] + '-report.md'
+		f = open(reportFile, 'wb')
+		f.write(self.report(course))
+		print('Report generated for ' + self.name)
+		return 0
+		
+		
+class Course():
+
+	def __init__(self):
+		self.data = []
+		
+	def assessments(self):
+		
+		assessments = []
+		for stud in self.roster():
+			assessList = Student(stud).assessments()
+			for assess in assessList:
+				assessments.append(assess)
+		return sorted(set(assessments))
+
+	def categories(self):
+		
+		categories = []
+		for stud in self.roster():
+			catList = Student(stud).categories()
+			for cat in catList:
+				categories.append(cat)
+		return sorted(set(categories))
+
+	def roster(self):
+		roster = []
+		for filename in os.listdir('./'):
+			if filename.split('.')[1] == 'csv':
+				roster.append(filename)
+		i = 0
+		for file in roster:
+			if file == 'studentTemplate.csv':
+				roster.pop(i)
+			i = i + 1
+		return roster
+			
